b59628f78ec610069f0bd4559cd5d8e51075936a
david-dimas333 <david.dimas333@gmail.com>
2016-09-27 16:54:34 +0200

    update about us and command line tools pages

--- extensions/thedigitalgarage/js/main.js
+++ extensions/thedigitalgarage/js/main.js
@@ -16,7 +16,7 @@ function changeText() {
     if($('.empty-state-message.empty-state-full-page').size() == 1){
       $('h1, p, span').each(function() {
         var text = $(this).html();
-        text = text.replace('OpenShift', 'The Digital Garage');
+        text = text.replace(/OpenShift/g, 'The Digital Garage');
         text = text.replace('to The', 'to the');
         text = text.replace('Create a project for your application.', 'To get started, simply create a project for your application by clicking the orange add project button in the upper right corner of this screen.');
         text = text.replace('visit the The Digital Garage', 'visit our');
@@ -28,22 +28,30 @@ function changeText() {
 }
 
 /*
-    Update AboutUs page
+    Update AboutUs & Command Line Tools page
 */
 function changeAboutusPage() {
   var interval = setTimeout(function(){
-    if($('.about').size() == 1){
+    if($('.about').size() == 1){ //For About Us Page
       $('.about-icon').remove();
       $('.about .col-md-9').css('width', '100%');
       $('h1, p, span').each(function() {
         var text = $(this).html();
         text = text.replace('OpenShift by Red Hat', 'The Digital Garage');
-        text = text.replace('OpenShift', 'The Digital Garage');
-        text = text.replace('OpenShift projects from a terminal', 'projects');
+        text = text.replace(/OpenShift/g, 'The Digital Garage');
         text = text.replace('https://openshift.com', 'http://www.thedigitalgarage.io');
         $(this).html(text);
       });
       $('#version').prev().html("<a href='http://www.thedigitalgarage.io' target='_blank'>The Digital Garage</a> is a Platform-as-a-Service (PaaS) that is built on Openshift Origin, Red Hat's open source platform. The Digital Garage helps the worldwide community of software developers develop, host and scale the next generation of innovative applications in a cloud environment.");
+    }
+
+    if($('.command-line').size() == 1){ //For Command Line Tools Page
+      $('h1, p, span').each(function() {
+        var text = $(this).html();
+        text = text.replace(/OpenShift/g, 'The Digital Garage');
+        text = text.replace('OpenShift projects from a terminal', 'projects');
+        $(this).html(text);
+      });
       $('.cli-download-label').replaceWith('<h2>Download <code>oc</code>:</h2>');
       $('.cli-download-link').html("Releases <i class='fa fa-external-link'></i>");
       $('.cli-download-link').attr("href", "https://github.com/openshift/origin/releases");
@@ -59,7 +67,7 @@ $(document).ready(function(){
   $('link[rel*="icon"]').attr("href", "./extensions/images/favicon.ico");
   $('link[type="image/png"]').attr("href", "./extensions/images/favicon.png");
 
-  //Update AboutUs page
+  //Update AboutUs & Command Line Tools page
   changeAboutusPage();
 
   //Change Text on first view
